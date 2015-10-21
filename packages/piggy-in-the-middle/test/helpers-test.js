9d2aed1a357230b2da4e27f653f8e3f74a034c7c
DaveJ <dave@davejeffery.com>
2015-10-21 01:05:44 +0100

    Add more tests

--- packages/piggy-in-the-middle/test/helpers-test.js
+++ packages/piggy-in-the-middle/test/helpers-test.js
@@ -8,32 +8,36 @@ function getPath(path) {
   return Path.join(process.cwd(), 'examples/site', path);
 }
 
-describe('findSourceFile', function () {
-  it('should find a source file if there is one (JS)', function () {
-    return expect(helpers.findSourceFile(getPath('scripts/main.js')),
-      'to be fulfilled with',
-      getPath('scripts/main.coffee')
-    );
+describe('helpers', function() {
+  
+  describe('findSourceFile', function () {
+    it('should find a source file if there is one (JS)', function () {
+      return expect(helpers.findSourceFile(getPath('scripts/main.js')),
+        'to be fulfilled with',
+        getPath('scripts/main.coffee')
+      );
+    });
+
+    it('should find a source file if there is one (CSS)', function () {
+      return expect(helpers.findSourceFile(getPath('styles/main.css')),
+        'to be fulfilled with',
+        getPath('styles/main.styl')
+      );
+    });
+
+    it('should reject with `null` when it can\'t find source file', function () {
+      return expect(helpers.findSourceFile(getPath('scripts/404.js')),
+        'to be rejected with',
+        null
+      );
+    });
+
+    it('should reject with `null` when path doesn\'t exist', function () {
+      return expect(helpers.findSourceFile(getPath('foo/bar')),
+        'to be rejected with',
+        null
+      );
+    });
   });
 
-  it('should find a source file if there is one (CSS)', function () {
-    return expect(helpers.findSourceFile(getPath('styles/main.css')),
-      'to be fulfilled with',
-      getPath('styles/main.styl')
-    );
-  });
-
-  it('should reject with `null` when it can\'t find source file', function () {
-    return expect(helpers.findSourceFile(getPath('scripts/404.js')),
-      'to be rejected with',
-      null
-    );
-  });
-
-  it('should reject with `null` when path doesn\'t exist', function () {
-    return expect(helpers.findSourceFile(getPath('foo/bar')),
-      'to be rejected with',
-      null
-    );
-  });
 });
---
+++ packages/piggy-in-the-middle/test/middleware-test.js
@@ -0,0 +1,156 @@
+'use strict';
+
+var Path = require('path');
+var expect = require('unexpected')
+  .clone()
+  .installPlugin(require('unexpected-express'))
+  .installPlugin(require('unexpected-sinon'));
+var connect = require('connect');
+var sinon = require('sinon');
+var pitm = require('../lib/piggy-in-the-middle');
+var babyTolk = require('baby-tolk');
+var fs = require('fs');
+
+
+function getPath(path) {
+  return Path.join(process.cwd(), 'examples/site', path || '');
+}
+
+describe('middleware', function () {
+
+
+  var app;
+  var compileCount = 0;
+
+  before(function() {
+    app = connect().use(pitm(getPath()));
+    babyTolk.read = sinon.spy(babyTolk, 'read');
+  });
+
+  function expectCompiled() {
+    compileCount = compileCount + 1;
+    return expect(babyTolk.read, 'was called times', compileCount);
+  }
+
+  function expectCached() {
+    return expect(babyTolk.read, 'was called times', compileCount);
+  }
+
+  describe('first requests', function() {
+
+    it('should compile jade file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/' },
+        response: {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'text/html; charset=UTF-8'
+          },
+          body: expect.it('to contain', '<h1>Piggy In The Middle</h1>')
+        }
+      }).then(() => expectCompiled());
+    });
+
+    it('should compile styl file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/styles/main.css' },
+        response: {
+          headers: {
+            'Content-Type': 'text/css; charset=UTF-8',
+            'X-SourceMap': 'main.css.map'
+          },
+          body: expect.it('to contain', 'body {')
+        }
+      }).then(() => expectCompiled());
+    });
+
+    it('should compile coffee file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/scripts/main.js' },
+        response: {
+          headers: {
+            'Content-Type': 'application/javascript',
+            'X-SourceMap': 'main.js.map'
+          },
+          body: expect.it('to contain', 'console.log(')
+        }
+      }).then(() => expectCompiled());
+    });
+  });
+
+  describe('second (cached) requests', function() {
+
+    it('should *not* compile jade file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/' },
+        response: {
+          headers: {
+            'Content-Type': 'text/html; charset=UTF-8'
+          },
+          body: expect.it('to contain', '<h1>Piggy In The Middle</h1>')
+        }
+      }).then(() => expectCached());
+    });
+
+    it('should *not* compile styl file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/styles/main.css' },
+        response: {
+          headers: {
+            'Content-Type': 'text/css; charset=UTF-8',
+            'X-SourceMap': 'main.css.map'
+          },
+          body: expect.it('to contain', 'body {')
+        }
+      }).then(() => expectCached());
+    });
+
+    it('should *not* compile coffee file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/scripts/main.js' },
+        response: {
+          headers: {
+            'Content-Type': 'application/javascript',
+            'X-SourceMap': 'main.js.map'
+          },
+          body: expect.it('to contain', 'console.log(')
+        }
+      }).then(() => expectCached());
+    });
+
+  });
+
+  describe('third requests (after editing source)', function() {
+    var pathToCSS = getPath('styles/main.styl');
+    var fileContents;
+
+    before(function(done) {
+      fs.readFile(pathToCSS, function(err, contents){
+        fileContents = contents;
+        fs.writeFile(pathToCSS, contents+' ', function() {
+          setTimeout(done, 250); // Allow 250ms for chokidar to notice the change
+        });
+      });
+    });
+
+    after(function(done) {
+      // Revert file back to original state
+      fs.writeFile(pathToCSS, fileContents, done);
+    });
+
+    it('should recompile styl file', function() {
+      return expect(app, 'to yield exchange', {
+        request: { url: '/styles/main.css' },
+        response: {
+          headers: {
+            'Content-Type': 'text/css; charset=UTF-8',
+            'X-SourceMap': 'main.css.map'
+          },
+          body: expect.it('to contain', 'body {')
+        }
+      }).then(() => expectCompiled());
+    });
+
+  });
+
+});
