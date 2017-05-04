27f28bba5cc24650b75d184ef8eafa1a63c81e0a
DaveJ <dave@davejeffery.com>
2017-05-04 16:36:12 +0100

    Lint and add support for ejs, pug, buble and Sass with the .sass extension

--- packages/compile/lib/baby-tolk.js
+++ packages/compile/lib/baby-tolk.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if (global && global.babyTolkCompilerModulePath) {
-  require('app-module-path').addPath(global.babyTolkCompilerModulePath);
-}
-
 var Path = require('path');
 var when = require('when');
 var node = require('when/node');
@@ -22,12 +18,15 @@ var sourceExtension;
 var adapters = [
   'LiveScript',
   'babel',
+  'buble',
   'coco',
   'coffee-script',
   'dogescript',
+  'ejs',
   'less',
   'markdown',
   'myth',
+  'pug',
   'scss',
   'stylus',
   'swig',
@@ -37,50 +36,54 @@ var adapters = [
 function load() {
   extensionMap = {};
   loadedAdapters = [];
-
-  Object.keys(accord.all()).map(function (engine) {
-    if (adapters.indexOf(engine) === -1) {
-      return undefined;
-    }
-
-    try {
-      return accord.load(engine, global && global.babyTolkCompilerModulePath || null);
-    } catch (e) {
-      if (e.code !== 'MODULE_NOT_FOUND' && e.message.indexOf('Cannot find module') === -1) {
-        console.error(e.message.replace(/^error: ?/i, 'Accord Error: ') + '. Try updating to the latest version');
-      }
-      // else {
-      //   console.error('Missing adapter:', engine);
-      // }
-    }
-  }).filter(function (engine) {
-    return engine;
-  }).forEach(function (adapter) {
-    if (adapter.engineName === 'babel' || adapter.engineName === 'babel-core') {
-      // Monkey-patching Babel adapter so that it doesn't try and compile all .js files
-      adapter.extensions = ['babel.js'];
-    }
-    loadedAdapters.push(adapter);
-    var extensions = adapter.extensions.map(function (extension) { return '.' + extension; });
-
-    extensions.forEach(function (extension) {
-      if (!Array.isArray(extensionMap[extension])) {
-        extensionMap[extension] = [];
+  Object.keys(accord.all())
+    .map((engine) => {
+      if (adapters.indexOf(engine) === -1) {
+        return undefined;
       }
 
-      extensionMap[extension].push(adapter);
+      try {
+        return accord.load(engine, global ? global.babyTolkCompilerModulePath : null);
+      } catch (e) {
+        if (e.code !== 'MODULE_NOT_FOUND' && e.message.indexOf('Cannot find module') === -1) {
+          console.error(
+            `${e.message.replace(/^error: ?/i, 'Accord Error: ')}. Try updating to the latest version`
+          );
+        }
+        // else {
+        //   console.error('Missing adapter:', engine);
+        // }
+      }
+    })
+    .filter(engine => engine)
+    .forEach((adapter) => {
+      if (adapter.engineName === 'babel' || adapter.engineName === 'babel-core') {
+        // Monkey-patching Babel adapter so that it doesn't try and compile all .js files
+        adapter.extensions = ['babel.js'];
+      }
+      if (adapter.engineName === 'buble') {
+        // Monkey-patching Bublé adapter so that it doesn't try and compile all .js files
+        adapter.extensions = ['buble.js'];
+      }
+      loadedAdapters.push(adapter);
+      var extensions = adapter.extensions.map(extension => `.${extension}`);
+      extensions.forEach((extension) => {
+        if (!Array.isArray(extensionMap[extension])) {
+          extensionMap[extension] = [];
+        }
+
+        extensionMap[extension].push(adapter);
+      });
     });
-  });
 
   targetExtension = {};
   sourceExtension = {};
 
-  Object.keys(extensionMap).forEach(function (sourceExt) {
+  Object.keys(extensionMap).forEach((sourceExt) => {
     var adapters = extensionMap[sourceExt];
-    var targetExt = '.' + adapters[0].output;
+    var targetExt = `.${adapters[0].output}`;
 
     targetExtension[sourceExt] = targetExt;
-
 
     if (!sourceExtension[targetExt]) {
       sourceExtension[targetExt] = [];
@@ -98,15 +101,15 @@ var dontCompile = function (pathName) {
   return baseName[0] === '_';
 };
 
-var createHash = function(data) {
+var createHash = function (data) {
   var shasum = crypto.createHash('sha1');
   shasum.update(data);
   return shasum.digest('hex');
 };
 
-var sanitizeOptions = function(options) {
+var sanitizeOptions = function (options) {
   options = options || {};
-  options.sourceMap = options.sourceMap === false ? false : true;
+  options.sourceMap = options.sourceMap !== false;
   if (options.sha) {
     options.inputSha = true;
     options.outputSha = true;
@@ -114,22 +117,26 @@ var sanitizeOptions = function(options) {
   return options;
 };
 
-var getAdapter = function(extension) {
+var getAdapter = function (extension) {
   var adapters = extensionMap[extension];
   return adapters && adapters[0];
 };
 
-var _getTransformId = function(adapter, options) {
+var _getTransformId = function (adapter, options) {
   var transformId = '';
   if (adapter) {
-    transformId = adapter.engineName + '@' + adapter.engine.version;
+    transformId = `${adapter.engineName}@${adapter.engine.version}`;
   }
-  if (options.sourceMap) { transformId += '::map'; }
-  if (options.minify) { transformId += '::minify'; }
+  if (options.sourceMap) {
+    transformId += '::map';
+  }
+  if (options.minify) {
+    transformId += '::minify';
+  }
   return transformId;
 };
 
-var getTransformId = function(pathName, options) {
+var getTransformId = function (pathName, options) {
   options = sanitizeOptions(options);
   var extension = pathCompleteExtname(pathName);
   var adapter = !dontCompile(pathName) && getAdapter(extension);
@@ -149,8 +156,8 @@ module.exports = {
   reload: load,
   adapters: loadedAdapters,
   isMinifiable: minify.isMinifiable,
-  getTransformId: getTransformId,
-  read: function (pathName, options) {
+  getTransformId,
+  read(pathName, options) {
     options = sanitizeOptions(options);
 
     var extension = pathCompleteExtname(pathName);
@@ -159,64 +166,74 @@ module.exports = {
     var sourceFile = fs.readFile(pathName, 'utf8');
     var sourceFileContents = null;
 
-    var continuation = sourceFile.then(function(result) {
-      var obj = { result: result };
+    var continuation = sourceFile.then((result) => {
+      var obj = { result };
       sourceFileContents = result;
       // if (options.inputSha) { obj.inputSha = createHash(result); }
       return obj;
     });
 
-
     var transformId = _getTransformId(adapter, options);
     if (adapter) {
       var transpilerOptions = Object.assign({}, options, {
         sourcemap: options.sourceMap,
-        filename: pathName
+        filename: pathName,
       });
 
       if (adapter.engineName === 'node-sass') {
         transpilerOptions.includePaths = [Path.dirname(pathName)];
+        if (extension === '.sass') transpilerOptions.indentedSyntax = true;
       }
 
-      continuation = continuation.then(function(source) {
-        return adapter.render(source.result, transpilerOptions)
-          .then(function(compiled) {
-            compiled.extension = '.' + adapter.output;
-            compiled.inputPath = pathName;
-            if (options.inputSha) { compiled.inputSha = source.inputSha; }
-            return compiled;
-          });
-      });
-
+      continuation = continuation.then(source =>
+        adapter.render(source.result, transpilerOptions).then((compiled) => {
+          compiled.extension = `.${adapter.output}`;
+          compiled.inputPath = pathName;
+          if (options.inputSha) {
+            compiled.inputSha = source.inputSha;
+          }
+          return compiled;
+        })
+      );
     } else {
-      continuation = continuation.then(function(source) {
+      continuation = continuation.then((source) => {
         source.extension = extension;
         source.inputPath = pathName;
         return source;
       });
     }
 
-    continuation = continuation.then(function(compiled) {
-      if (options.minify) { compiled = minify(compiled, options); }
-      if (options.outputSha) { compiled.outputSha = createHash(compiled.result); }
+    continuation = continuation.then((compiled) => {
+      if (options.minify) {
+        compiled = minify(compiled, options);
+      }
+      if (options.outputSha) {
+        compiled.outputSha = createHash(compiled.result);
+      }
       compiled.transformId = transformId === '' ? null : transformId;
       if (options.inputSha) {
-        var inputSha = [{
-          file: pathName,
-          sha: createHash(sourceFileContents)
-        }];
+        var inputSha = [
+          {
+            file: pathName,
+            sha: createHash(sourceFileContents),
+          }
+        ];
         if (compiled.sourcemap) {
-          inputSha = inputSha.concat(compiled.sourcemap.sources.map(function(path) {
-            if (path === pathName) { return null; }
-            return fs.readFile(path, 'utf8').then(function(content) {
-              return {
-                file: path,
-                sha: createHash(content)
-              };
-            });
-          }).filter(function(x) { return Boolean(x); }));
+          inputSha = inputSha.concat(
+            compiled.sourcemap.sources
+              .map((path) => {
+                if (path === pathName) {
+                  return null;
+                }
+                return fs.readFile(path, 'utf8').then(content => ({
+                  file: path,
+                  sha: createHash(content),
+                }));
+              })
+              .filter(x => Boolean(x))
+          );
         }
-        return when.all(inputSha).then(function(inputSha) {
+        return when.all(inputSha).then((inputSha) => {
           compiled.inputSha = inputSha;
           return compiled;
         });
@@ -225,5 +242,5 @@ module.exports = {
     });
 
     return continuation;
-  }
+  },
 };
