13e5c590087bb94d2ed57faf086c034d4112863d
DaveJ <dave@davejeffery.com>
2017-09-07 19:58:14 +0100

    Instant: Support injecting to HTML pages that don't have `</body>` tag

--- packages/instant/lib/inject.js
+++ packages/instant/lib/inject.js
@@ -1,18 +1,20 @@
-var tamper = require('tamper')
+const tamper = require('tamper')
 
 /**
  * Returns a middleware that injects a script tag with the given URL before
  * the closing body tag of text/html responses.
  */
-module.exports = function(script) {
-  return tamper(function(req, res) {
-    var url = req.realUrl || req.url
-      , mime = res.getHeader('Content-Type')
+module.exports = function (script) {
+  return tamper((req, res) => {
+    let url = req.realUrl || req.url,
+      mime = res.getHeader('Content-Type')
 
     if (/text\/html/.test(mime) && !/instant\/events/.test(url)) {
-      return function(body) {
-        return body.replace(/<\/body>/i,
-          '  <script src="' + script + '"></script>\n</body>')
+      return function (body) {
+        if (body.search(/<\/body>/i) !== -1) {
+          return body.replace(/<\/body>/i, `  <script src="${script}"></script>\n</body>`)
+        }
+        return `${body}  <script src="${script}"></script>`
       }
     }
   })
