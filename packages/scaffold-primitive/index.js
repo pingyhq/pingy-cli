5794228d07987494907385da1530c2956827cd07
DaveJ <dave@davejeffery.com>
2017-11-11 00:15:36 +0000

    [scaffold-primitive] add support for custom scaffolds

--- packages/scaffold-primitive/index.js
+++ packages/scaffold-primitive/index.js
@@ -7,45 +7,12 @@ const {
   tabTemplateAndOutput,
 } = require('./helpers');
 const dotPingyTmpl = require('./dotPingyTmpl');
-const { writeFileSync, readFileSync, existsSync } = require('fs');
+const { writeJson, pathExists, readJson } = require('fs-extra');
 const path = require('path');
 const checkdir = require('checkdir');
 
-function doFiles(dirToScaffoldFrom, dirToScaffoldTo, options) {
-  if (!Array.isArray(options.files)) return Promise.resolve([]);
-  return Promise.all(
-    options.files.map(fixPaths(dirToScaffoldFrom, dirToScaffoldTo)).map(addInputString)
-  ).then(objs =>
-    Promise.all(objs.map(tabTemplateAndOutput(options || {}))).then(returnOutputFilePath(objs))
-  );
-}
-
-function doInstall(installType, scaffoldOptions, options) {
-  return new Promise((resolve) => {
-    const pkgs = scaffoldOptions[installType];
-    if (!Array.isArray(pkgs)) return resolve(null);
-    const isDev = installType === 'devDependencies';
-    if (isDev && !options.globalPingy) pkgs.push('@pingy/cli');
-    if (pkgs.length === 0) return resolve(null);
-
-    let packageCmd = options.yarn ? 'yarn' : 'npm';
-    if (/^win/.test(process.platform)) packageCmd += '.cmd';
-    const packageOptions = [];
-    if (options.yarn) {
-      packageOptions.push('add');
-      if (isDev) packageOptions.push('--dev');
-    } else {
-      // npm
-      packageOptions.push('install');
-      if (isDev) packageOptions.push('--save-dev');
-      else packageOptions.push('--save');
-    }
-    return resolve({
-      cmd: packageCmd,
-      args: [...packageOptions, ...pkgs],
-    });
-  });
-}
+const tabbed = { spaces: '\t' };
+const filenameBlacklist = ['package.json', 'pingy.json', '.pingy.json'];
 
 module.exports.preflight = function preflight(dirToScaffoldFrom, dirToScaffoldTo, options) {
   return checkdir(dirToScaffoldTo, { ignoreDotFiles: true }).then((dirInfo) => {
@@ -62,51 +29,64 @@ module.exports.preflight = function preflight(dirToScaffoldFrom, dirToScaffoldTo
 };
 
 module.exports.npmInit = function npmInit(dirToScaffoldTo) {
-  return new Promise((resolve) => {
-    const pkgJsonPath = path.join(dirToScaffoldTo, 'package.json');
-    const pkgJsonExists = existsSync(pkgJsonPath);
-    if (pkgJsonExists) return resolve(null);
-    const npmCmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-    const subcommand = ['init', '--yes'];
-    return resolve({
-      cmd: npmCmd,
-      args: subcommand,
-    });
+  const pkgJsonPath = path.join(dirToScaffoldTo, 'package.json');
+  return pathExists(pkgJsonPath).then((pkgJsonExists) => {
+    if (pkgJsonExists) return null;
+    return {
+      cmd: 'npm',
+      args: ['init', '--yes'],
+    };
   });
 };
 
-module.exports.addPkgScripts = function addPkgScripts(dirToScaffoldTo) {
-  return new Promise((resolve) => {
-    const pkgJsonPath = path.join(dirToScaffoldTo, 'package.json');
-    const pkgJson = JSON.parse(readFileSync(pkgJsonPath, 'utf8'));
-    if (!pkgJson.scripts) pkgJson.scripts = {};
-    pkgJson.scripts.start = 'pingy dev';
-    pkgJson.scripts.export = 'pingy export';
-    writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, '\t'));
-    return resolve(pkgJson);
+module.exports.updatePkgJson = function addPkgScripts(dirToScaffoldTo, scaffoldOptions) {
+  const pkgJsonPath = path.join(dirToScaffoldTo, 'package.json');
+  return readJson(pkgJsonPath).then((pkgJson) => {
+    const newPkgJson = Object.assign({}, pkgJson, {
+      dependencies: Object.assign({}, pkgJson.dependencies, scaffoldOptions.dependencies),
+      devDependencies: Object.assign({}, pkgJson.devDependencies, scaffoldOptions.devDependencies),
+      scripts: Object.assign({}, pkgJson.scripts, {
+        start: 'pingy dev',
+        export: 'pingy export',
+      }),
+    });
+    return writeJson(pkgJsonPath, newPkgJson, tabbed);
   });
 };
 
-module.exports.addPingyJson = function addPingyJson(dirToScaffoldTo) {
-  return new Promise((resolve) => {
-    const filename = 'pingy.json';
-    const pingyJson = dotPingyTmpl({
-      name: path.basename(dirToScaffoldTo),
-    });
-    writeFileSync(path.join(dirToScaffoldTo, filename), JSON.stringify(pingyJson), 'utf8');
-    return resolve(pingyJson);
-  });
+module.exports.addPingyJson = function addPingyJson(dirToScaffoldTo, scaffoldOptions) {
+  const pingyJsonPath = path.join(dirToScaffoldTo, 'pingy.json');
+  const dirname = path.basename(dirToScaffoldTo);
+  const pingyJson = dotPingyTmpl(
+    Object.assign(
+      {
+        name: dirname,
+      },
+      scaffoldOptions.pingyJson
+    )
+  );
+  return writeJson(pingyJsonPath, pingyJson, tabbed);
 };
 
 module.exports.scaffold = function scaffold(dirToScaffoldFrom, dirToScaffoldTo, options) {
   if (typeof options !== 'object') return Promise.resolve([]);
-  return doFiles(dirToScaffoldFrom, dirToScaffoldTo, options);
+  if (!Array.isArray(options.files)) return Promise.resolve([]);
+  return Promise.all(
+    options.files
+      .filter(x => !filenameBlacklist.includes(x.output))
+      .map(fixPaths(dirToScaffoldFrom, dirToScaffoldTo))
+      .map(addInputString)
+  ).then(objs =>
+    Promise.all(objs.map(tabTemplateAndOutput(options || {}))).then(returnOutputFilePath(objs))
+  );
 };
 
-module.exports.install = function install(scaffoldOptions, options) {
-  return doInstall('dependencies', scaffoldOptions, options);
-};
-
-module.exports.installDev = function installDev(scaffoldOptions, options) {
-  return doInstall('devDependencies', scaffoldOptions, options);
+module.exports.install = function install(dirToScaffoldTo, scaffoldOptions, options) {
+  const pkgJsonPath = path.join(dirToScaffoldTo, 'package.json');
+  return readJson(pkgJsonPath).then(pkgJson => ({
+    dependencies: pkgJson.dependencies,
+    devDependencies: pkgJson.devDependencies,
+    cmd: options.yarn ? 'yarn' : 'npm',
+    args: ['install'],
+  }));
 };
